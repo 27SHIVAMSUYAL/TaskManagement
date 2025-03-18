d2a649129879ba39fb2ed4c73dc80981a50991f9
27SHIVAMSUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-03-19 03:13:31 +0530

    Delete frontend directory

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure Tailwind scans all React components
-    theme: {
-      extend: {}, // You can add custom themes here if needed
-    },
-    plugins: [], // You can add Tailwind plugins here if required
-  };
-  
